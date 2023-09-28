7659d5f5f0ad0f2a488fa87a271cd2a7e96f22d6
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-09-28 15:04:54 +0200

    Fix:Implemented Strongly Typed Views on Author model.-Added portuguese display name-Added Customized messages on CRUD views

--- SGBWeb/Models/Author.cs
+++ SGBWeb/Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,17 @@ namespace SGBWeb.Models
     public class Author
     {
         public int ID { get; set; }
+        [DisplayName("Nome")]
         public string AuthorName { get; set; }
+        [DisplayName("Data Nascimento")]
         public DateTime DateOfBirth { get; set; }
+        [DisplayName("País")]
         public string CountryOfOrigin { get; set; }
+        [DisplayName("Biografia")]
         public string Biography { get; set; } // Author's biography
         public string Website { get; set; }    // Author's website URL
         public string Email { get; set; }      // Author's contact email
+        [DisplayName("Contacto")]
         public string Phone { get; set; }      // Author's contact phone number
                                                // Add any other relevant author information properties here
     }
