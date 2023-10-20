c70c8fc03acb34535c038d65924cb6a18915c659
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-10-20 17:56:46 +0200

    -Adding Publisher Model -Adding PublisherService -Introduce GetAllPublishers method for fetching a list of all publishers on Publisher Controller.

---
+++ SGBWeb/Models/Publisher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SGBWeb.Models
+{
+    [Table("Publishers")]
+    public class Publisher
+    {
+        [Key]
+        public int PublisherID { get; set; }
+
+        [Required]
+        [Display(Name = "Nome do Editora")]
+        public string PublisherName { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string Address { get; set; }
+
+        [Display(Name = "Telefone")]
+        public string Phone { get; set; }
+    }
+
+}
---
+++ SGBWeb/Services/PublisherService.cs
@@ -0,0 +1,67 @@
+﻿using SGBWeb.Data;
+using SGBWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGBWeb.Services
+{
+    public class PublisherService
+    {
+        private readonly LibraryDbContext _dbContext; // Replace YourDbContext with your actual Entity Framework DbContext
+
+        public PublisherService()
+        {
+            _dbContext = new LibraryDbContext();
+        }
+
+        // Create a new publisher
+        public Publisher CreatePublisher(Publisher publisher)
+        {
+            _dbContext.Publishers.Add(publisher);
+            _dbContext.SaveChanges();
+            return publisher;
+        }
+
+        // Read (Retrieve) a publisher by ID
+        public Publisher GetPublisherById(int publisherId)
+        {
+            return _dbContext.Publishers.Find(publisherId);
+        }
+
+        // Read (Retrieve) all publishers
+        public List<Publisher> GetAllPublishers()
+        {
+            return _dbContext.Publishers.ToList();
+        }
+
+        // Update an existing publisher
+        public Publisher UpdatePublisher(int publisherId, Publisher updatedPublisher)
+        {
+            var existingPublisher = _dbContext.Publishers.Find(publisherId);
+            if (existingPublisher != null)
+            {
+                existingPublisher.PublisherName = updatedPublisher.PublisherName;
+                existingPublisher.Address = updatedPublisher.Address;
+                existingPublisher.Phone = updatedPublisher.Phone;
+                _dbContext.SaveChanges();
+            }
+            return existingPublisher;
+        }
+
+        // Delete a publisher by ID
+        public bool DeletePublisher(int publisherId)
+        {
+            var publisherToDelete = _dbContext.Publishers.Find(publisherId);
+            if (publisherToDelete != null)
+            {
+                _dbContext.Publishers.Remove(publisherToDelete);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
