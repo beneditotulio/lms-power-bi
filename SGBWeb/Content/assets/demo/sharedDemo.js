1ab6c885493ca17d3a775d890e8c77f74e66e521
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-10-21 13:48:11 +0200

    Added Startbootstrap template

---
+++ SGBWeb/Content/assets/demo/sharedDemo.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+
+    //Input Money Mask
+    let input = SimpleMaskMoney.setMask('#price'); 
+});
+
+let input = SimpleMaskMoney.setMask('#price',{
+        prefix: '',
+        suffix: '',
+        fixed: true,
+        fractionDigits: 2,
+        decimalSeparator: ',',
+        thousandsSeparator: '.',
+        emptyOrInvalid: () => {
+          return this.SimpleMaskMoney.args.fixed
+            ? `0${this.SimpleMaskMoney.args.decimalSeparator}00`
+            : `_${this.SimpleMaskMoney.args.decimalSeparator}__`;
+        }
+        });
