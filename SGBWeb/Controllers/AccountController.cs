bbcb73641a6c0a1e070af0b262f7a9625c03682f
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2023-12-15 23:18:59 +0200

    Feat: Add feature to retrieve book copies by ISBN
    Implement a new controller action 'GetBookCopies' in the 'Books' controller, which takes an ISBN parameter and retrieves a list of book copies associated with the provided ISBN. The action calls the 'GetBookCopiesByISBN' method from the 'BookService' to fetch the copies from the database. The 'GetBookCopiesByISBN' method in the service interacts with the database context, filtering copies based on the given ISBN and including related book information. This enhancement provides a way to view and manage book copies associated with a specific ISBN.

--- SGBWeb/Controllers/AccountController.cs
+++ SGBWeb/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace SGBWeb.Controllers
 
         public AccountController()
         {
+
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
