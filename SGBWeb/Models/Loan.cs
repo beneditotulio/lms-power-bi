899b75373899547f4c0e4c587b6ddd6cfe23d9fd
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2024-02-15 13:19:50 +0200

    Title: Enhance Loan views with navigation links and data validation
    Body:
    - Updated Loan views to include navigation links back to the Index view for better user experience.
    - Integrated client-side and server-side validation in Create and Edit forms to enforce data integrity.
    - Adjusted layout and styling of Loan views for consistency with the application's overall design.

---
+++ SGBWeb/Models/Loan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGBWeb.Models
+{
+    [Table("Loans")]
+    public class Loan
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID do Empréstimo")]
+        public int LoanID { get; set; }
+
+        //[ForeignKey("Book")]
+        [DisplayName("ISBN")]
+        public string ISBN { get; set; }
+
+        //[ForeignKey("Copy")]
+        [DisplayName("ID da Cópia")]
+        public int CopyID { get; set; }
+
+        //[ForeignKey("Member")]
+        [DisplayName("ID do Membro")]
+        public string MemberID { get; set; }
+
+        //[ForeignKey("User")]
+        [DisplayName("ID do Usuário")]
+        public string UserId { get; set; }
+
+        [DisplayName("Data do Empréstimo")]
+        public DateTime LoanDate { get; set; }
+
+        [DisplayName("Data de Vencimento")]
+        public DateTime DueDate { get; set; }
+
+        [DisplayName("Data de Devolução")]
+        public DateTime? ReturnedDate { get; set; }
+
+        // Navigation properties
+        //public virtual Book Book { get; set; }
+        //public virtual Copy Copy { get; set; }
+        //public virtual Member Member { get; set; }
+        //public virtual Member User { get; set; } // Assuming
+
+    }
+}
