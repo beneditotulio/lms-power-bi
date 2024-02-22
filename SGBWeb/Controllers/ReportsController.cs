3686ed93e416c50102837b72633481e418eddcba
Túlio Benedito Nhantumbo <beneditotulio28@gmail.com>
2024-02-22 23:36:40 +0200

    Feat: Added Student Power BI Report
    Details:
    -Created Report Controller to manage power BI reports
    -Embeded Student Report

---
+++ SGBWeb/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGBWeb.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student Report
+        public ActionResult StudentReport()
+        {
+            return View();
+        }
+    }
+}
